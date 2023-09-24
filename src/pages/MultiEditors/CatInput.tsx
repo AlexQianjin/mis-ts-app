502e16c2fb0ecde5203bf26b4d965121196afdfc
Alex Qin <qianjin.qin@qq.com>
2023-09-24 15:02:51 +0800

    add redux toolkit and tailwind css

--- src/pages/MultiEditors/CatInput.tsx
+++ src/pages/MultiEditors/CatInput.tsx
@@ -1,34 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TextField, Button } from '@mui/material';
 import { Cat } from './Cat';
 
 interface CatInputProps {
-    cat: Cat;
-    index: number;
-    onCatChange: (index: number, cat: Cat) => void;
-    onCatRemove: (index: number) => void;
-};
+  cat: Cat;
+  index: number;
+  onCatChange: (index: number, cat: Cat) => void;
+  onCatRemove: (index: number) => void;
+}
 
 export default function CatInput({ cat, index, onCatChange, onCatRemove }: CatInputProps) {
-    const hanldeChange = (e) => {
-        const changedCat = { ...cat, [e.target.name]: e.target.value }
-        console.log(changedCat, 15);
-        onCatChange(index, changedCat);
-    };
+  const hanldeChange = (e) => {
+    const changedCat = { ...cat, [e.target.name]: e.target.value };
+    console.log(changedCat, 15);
+    onCatChange(index, changedCat);
+  };
 
-    const handleRemove = () => {
-        onCatRemove(index);
-    };
+  const handleRemove = () => {
+    onCatRemove(index);
+  };
 
-    console.log(cat, 22);
+  console.log(cat, 22);
 
-    return (
-        <div>
-            <label>Name: </label>
-            <TextField name={'name'} value={cat.name} onChange={hanldeChange} error={false} helperText={'name error'}/>
-            <label>Age: </label>
-            <TextField name={'age'} value={cat.age} onChange={hanldeChange} error helperText={'age error'}/>
-            <Button onClick={handleRemove}>Remove</Button>
-        </div>
-    );
-};
+  return (
+    <form>
+      <label htmlFor="name">Name: </label>
+      <TextField
+        id="name"
+        name="name"
+        value={cat.name}
+        onChange={hanldeChange}
+        error={false}
+        helperText="name error"
+      />
+      <label htmlFor="age">Age: </label>
+      <TextField
+        id="age"
+        name="age"
+        value={cat.age}
+        onChange={hanldeChange}
+        error
+        helperText="age error"
+      />
+      <Button onClick={handleRemove}>Remove</Button>
+    </form>
+  );
+}
--- src/pages/MultiEditors/index.tsx
+++ src/pages/MultiEditors/index.tsx
@@ -4,37 +4,46 @@ import { Cat } from './Cat';
 import CatInput from './CatInput';
 
 export default function MultiEditors() {
-    const initialCats: Cat[] = [];
-    const [cats, setCats] = useState(initialCats);
+  const initialCats: Cat[] = [];
+  const [cats, setCats] = useState(initialCats);
 
-    const onCatChange = (index: number, cat: Cat) => {
-        console.log(index, 9);
-        console.log(cat, 10);
-        let newCats = [...cats];
-        newCats[index] = cat;
-        setCats(newCats);
-    };
+  const onCatChange = (index: number, cat: Cat) => {
+    console.log(index, 9);
+    console.log(cat, 10);
+    const newCats = [...cats];
+    newCats[index] = cat;
+    setCats(newCats);
+  };
 
-    const onCatRemove = (index: number) => {
-        let leftCats = cats;
-        leftCats.splice(index, 1);
-        console.log(index, 13);
-        setCats([...leftCats]);
-    };
+  const onCatRemove = (index: number) => {
+    const leftCats = cats;
+    leftCats.splice(index, 1);
+    console.log(index, 13);
+    setCats([...leftCats]);
+  };
 
-    const onCatAdd = () => {
-        setCats([...cats, { name: '', age: '' }]);
-    };
+  const onCatAdd = () => {
+    setCats([...cats, { name: '', age: '' }]);
+  };
 
-    return (
-        <div>
-            <label>Editors</label>
-            <div>
-                {cats.length > 0 ? cats.map((cat, index) => {
-                    return <CatInput key={`cat-${index}`} cat={cat} index={index} onCatChange={onCatChange} onCatRemove={onCatRemove} />
-                }) : 'No Cat'}
-            </div>
-            <Button onClick={onCatAdd}>Add</Button>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h3>Editors</h3>
+      <div>
+        {cats.length > 0
+          ? cats.map((cat, index) => (
+              <CatInput
+                key={`cat-${cat.name}-${cat.age}`}
+                cat={cat}
+                index={index}
+                onCatChange={onCatChange}
+                onCatRemove={onCatRemove}
+              />
+            ))
+          : 'No Cat'}
+      </div>
+      <Button onClick={onCatAdd}>Add</Button>
+    </div>
+  );
 }
